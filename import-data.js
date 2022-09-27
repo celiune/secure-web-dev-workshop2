f1cbf09c0924567ec7aac8cdb513e1c8f7c89011
celiune <celineliu1811@gmail.com>
2022-09-27 20:17:16 +0200

    Début du TD

--- import-data.js
+++ import-data.js
@@ -1,26 +1,30 @@
 require('dotenv').config()
-console.log(process.env.MONGO_URI)
-
 
 const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI).then(() => {console.log('Connected !') 
+console.log('Toto')})
+
+
+
 const { Schema } = mongoose;
 
-const blogSchema = new Schema({
+const filmSchema = new Schema({
     filmType: String,
     filmProducerName: String,
     endDate: Date,
     filmName: String,
     district: Number,
     geolocation: {
-      coordinates: [
-        Number,
-        Number
-      ],
+      coordinates: [Number],
       type: String
     },
-    sourceLocationId: Date,
+    sourceLocationId: String,
     filmDirectorName: String,
     address: String,
     startDate: Date,
     year: Number,
 });
+
+const Location = mongoose.model("Location",filmSchema)
+const maPremiereLocation = new Location ({filmType:'Horror'})
+await maPremiereLocation.save()
